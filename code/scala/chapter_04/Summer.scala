6325b00d2735450507d181281f51e479aec35ffc
Felipe M. Vieira <fmv1992@gmail.com>
2018-01-21 20:27:16 -0200

    got a working version just like the book

---
+++ code/scala/chapter_04/Summer.scala
@@ -0,0 +1,11 @@
+// In file Summer.scala
+import ChecksumAccumulator.calculate
+
+object Summer {
+
+  def main(args: Array[String]) = {
+    for (arg <- args)
+      println(arg + ": " + calculate(arg))
+  }
+
+}
