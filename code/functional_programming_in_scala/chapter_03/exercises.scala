2376e05a2332e310abf96ae54097e86e36d89c90
Felipe M. Vieira <fmv1992@gmail.com>
2018-03-17 17:19:56 -0300

    [fp in scala] uncommented previous exercises; build the List object incrementally per exercise

--- code/functional_programming_in_scala/chapter_03/exercises.scala
+++ code/functional_programming_in_scala/chapter_03/exercises.scala
@@ -2,36 +2,6 @@ import my_scala_project.Timer
 
 def PrintExercise(x: Any) = println("Exercise " + x.toString + ".")
 
-// Helper code. --- {{{
-// package fpinscala.datastructures
-
-//// sealed trait List[+A]
-////
-//// case object Nil extends List[Nothing]
-////
-//// case class Cons[+A](head: A, tail: List[A]) extends List[A]
-////
-//// def sum(ints: List[Int]): Int = ints match {
-////   case Nil => 0
-////   case Cons(x,xs) => x + sum(xs)
-//// }
-////
-//// def product(ds: List[Double]): Double = ds match {
-////   case Nil => 1.0
-////   case Cons(0.0, _) => 0.0
-////   case Cons(x,xs) => x * product(xs)
-//// }
-////
-//// val x = List(1,2,3,4,5) match {
-////   case Cons(x, Cons(2, Cons(4, _))) => x
-////   case Nil => 42
-////   case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
-////   case Cons(h, t) => h + sum(t)
-////   case _ => 101
-//// }
-
-// --- }}}
-
 // Exercise 3.1 --- {{{
 
 // case Cons(x, Cons(2, Cons(4, _))) => x
@@ -62,107 +32,107 @@ PrintExercise(3.1)
 
 // Exercise 3.2 --- {{{
 
-//// //  Implement the function tail for removing the first element of a List . Note
-//// //  that the function takes constant time. What are different choices you could
-//// //  make in your implementation if the List is Nil ? We’ll return to this
-//// //  question in the next chapter.
-////
-//// PrintExercise(3.2)
-////
-//// sealed trait List[+A]
-////
-//// case object Nil extends List[Nothing]
-////
-//// case class Cons[+A](head: A, tail: List[A]) extends List[A]
-////
-//// object List{
-////
-////   def sum(ints: List[Int]): Int = ints match {
-////     case Nil => 0
-////     case Cons(x,xs) => x + sum(xs)
-////   }
-////
-////   def product(ds: List[Double]): Double = ds match {
-////     case Nil => 1.0
-////     case Cons(0.0, _) => 0.0
-////     case Cons(x,xs) => x * product(xs)
-////   }
-////
-////   def apply[A](as: A*): List[A] =
-////     if (as.isEmpty) Nil
-////     else Cons(as.head, apply(as.tail: _*))
-////
-////   // It took me so long to go from this to the correct definition.
-////   //
-////   // def tail[A](list: A): List[A] = list match {
-////   //                   |.-> WRONG!
-////   def tail[A](list: List[A]): List[A] = list match {
-////     case Nil => Nil
-////     case Cons(h, t) => t
-////   }
-//// }
-////
-//// val l1 = List(1, 10, 100)
-//// println("List is: " + l1 + "\nTail is: " + List.tail(l1))
-////
-//// // The flexibility is what to do when the list is 'Nil'. We could raise an
-//// // exception for instance or just return Nil.
-////
-//// // TODO: There is no way to check that this function runs in linear time. Write
-//// // an object to do that (see the "Timer" object defined in
-//// // ../../my_scala_project).
+//  Implement the function tail for removing the first element of a ListE2 . Note
+//  that the function takes constant time. What are different choices you could
+//  make in your implementation if the ListE2 is NilE2 ? We’ll return to this
+//  question in the next chapter.
+
+PrintExercise(3.2)
+
+sealed trait ListE2[+A]
+
+case object NilE2 extends ListE2[Nothing]
+
+case class ConsE2[+A](head: A, tail: ListE2[A]) extends ListE2[A]
+
+object ListE2{
+
+  def sum(ints: ListE2[Int]): Int = ints match {
+    case NilE2 => 0
+    case ConsE2(x,xs) => x + sum(xs)
+  }
+
+  def product(ds: ListE2[Double]): Double = ds match {
+    case NilE2 => 1.0
+    case ConsE2(0.0, _) => 0.0
+    case ConsE2(x,xs) => x * product(xs)
+  }
+
+  def apply[A](as: A*): ListE2[A] =
+    if (as.isEmpty) NilE2
+    else ConsE2(as.head, apply(as.tail: _*))
+
+  // It took me so long to go from this to the correct definition.
+  //
+  // def tail[A](list: A): ListE2[A] = list match {
+  //                   |.-> WRONG!
+  def tail[A](list: ListE2[A]): ListE2[A] = list match {
+    case NilE2 => NilE2
+    case ConsE2(h, t) => t
+  }
+}
+
+val l1 = ListE2(1, 10, 100)
+println("ListE2 is: " + l1 + "\nTail is: " + ListE2.tail(l1))
+
+// The flexibility is what to do when the list is 'NilE2'. We could raise an
+// exception for instance or just return NilE2.
+
+// TODO: There is no way to check that this function runs in linear time. Write
+// an object to do that (see the "Timer" object defined in
+// ../../my_scala_project).
 
 
 // --- }}}
 
-//// // Exercise 3.3 --- {{{
-////
-//// //  Implement the function tail for removing the first element of a List . Note
-//// //  that the function takes constant time. What are different choices you could
-//// //  make in your implementation if the List is Nil ? We’ll return to this
-//// //  question in the next chapter.
-////
-//// PrintExercise(3.3)
-////
-//// sealed trait ListE3[+A]
-////
-//// case object NilE3 extends ListE3[Nothing]
-////
-//// case class ConsE3[+A](head: A, tail: ListE3[A]) extends ListE3[A]
-////
-//// object ListE3 {
-////
-////   def sum(ints: ListE3[Int]): Int = ints match {
-////     case NilE3 => 0
-////     case ConsE3(x,xs) => x + sum(xs)
-////   }
-////
-////   def product(ds: ListE3[Double]): Double = ds match {
-////     case NilE3 => 1.0
-////     case ConsE3(0.0, _) => 0.0
-////     case ConsE3(x,xs) => x * product(xs)
-////   }
-////
-////   def apply[A](as: A*): ListE3[A] = {
-////     if (as.isEmpty) NilE3
-////     else ConsE3(as.head, apply(as.tail: _*))
-////   }
-////
-////   def tail[A](list: ListE3[A]): ListE3[A] = list match {
-////     case NilE3 => NilE3
-////     case ConsE3(h, t) => t
-////   }
-////
-////   def setHead[A](new_element: A, list: ListE3[A]): ListE3[A] = list match {
-////     case NilE3 => ConsE3(new_element, NilE3)
-////     case ConsE3(h, t) => ConsE3(new_element, ConsE3(h, t))
-////   }
-//// }
-////
-//// val ex3 = ListE3(10, 150, 1)
-//// println(ListE3.setHead(999, ex3))
-////
-//// // --- }}}
+// Exercise 3.3 --- {{{
+
+//  Implement the function tail for removing the first element of a List . Note
+//  that the function takes constant time. What are different choices you could
+//  make in your implementation if the List is Nil ? We’ll return to this
+//  question in the next chapter.
+
+PrintExercise(3.3)
+
+sealed trait ListE3[+A]
+
+case object NilE3 extends ListE3[Nothing]
+
+case class ConsE3[+A](head: A, tail: ListE3[A]) extends ListE3[A]
+
+object ListE3 {
+
+  def sum(ints: ListE3[Int]): Int = ints match {
+    case NilE3 => 0
+    case ConsE3(x,xs) => x + sum(xs)
+  }
+
+  def product(ds: ListE3[Double]): Double = ds match {
+    case NilE3 => 1.0
+    case ConsE3(0.0, _) => 0.0
+    case ConsE3(x,xs) => x * product(xs)
+  }
+
+  def apply[A](as: A*): ListE3[A] = {
+    if (as.isEmpty) NilE3
+    else ConsE3(as.head, apply(as.tail: _*))
+  }
+
+  def tail[A](list: ListE3[A]): ListE3[A] = list match {
+    case NilE3 => NilE3
+    case ConsE3(h, t) => t
+  }
+
+  def setHead[A](new_element: A, list: ListE3[A]): ListE3[A] = list match {
+    case NilE3 => ConsE3(new_element, NilE3)
+    case ConsE3(h, t) => ConsE3(new_element, ConsE3(h, t))
+  }
+}
+
+val ex3 = ListE3(10, 150, 1)
+println(ListE3.setHead(999, ex3))
+
+// --- }}}
 
 // Exercise 3.4 --- {{{
 
@@ -265,14 +235,3 @@ val ex5 = ListE5(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 1)
 println(ListE5.dropWhile(ex5, (x: Int) => x % 9 == 0))
 
 // --- }}}
-
-//// // Tests. --- {{{
-//// val test = List(1, 3, 7)
-//// val test_tail = List.tail(test)
-//// val test_head = List.setHead(40, test)
-////
-//// println(test_tail)
-//// println(test_head)
-//// val timer = Timer
-//// println(timer.getRunningTimes((x: Int) => 2 * x, Range(0, 1000).toList))
-//// // --- }}}
