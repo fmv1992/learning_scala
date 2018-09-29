21086e4e4a44fafd3d23db52edf7d92aa9c48002
Felipe M. Vieira <fmv1992@gmail.com>
2018-09-29 14:40:38 -0300

    added code on words1_play_on_words

--- code/spoj/words1_play_on_words/words1_play_on_words.scala
+++ code/spoj/words1_play_on_words/words1_play_on_words.scala
@@ -1,35 +1,76 @@
-import scala.io.StdIn
 // See this topic for Scala discussion:
 // http://discuss.spoj.com/t/spoj-in-scala/7127
+import scala.io.StdIn
 
 // spoj: The program compiled successfully, but Main.class was not found.
 //       Class Main should contain method: def main(args: Array[String]).
-object Main extends App {
+object Main extends App { // --- {{{
   EntryPoint.main(Array(""))
-}
+} // --- }}}
 
 // Rules:
 // 1. Every word begins with the same letter as the previous word ends.
 // 2. All the plates from the list must be used, each exactly once.
 
-object Answer {
+case class Point(start: Int, end: Int) { // --- {{{
+
+} // --- }}}
+
+object Answer { // --- {{{
 
   val answer = Map(false -> "The door cannot be opened.",
     true -> "Ordering is possible.")
 
   def answer(ans: Vector[Boolean]): Unit = {
 
-    ans.foreach(x => println(Answer.answer(x)))
+    // ans.foreach(x => println(Answer.answer(x)))
 
   }
-}
+} // --- }}}
 
-object EntryPoint {
+object EntryPoint { // --- {{{
 
   def main(args: Array[String]): Unit = {
     val parsedInput = parseStdin
-    val contiguousSections = parsedInput.map(isContiguous)
-    Answer.answer(contiguousSections)
+    val walkablePaths = parsedInput.map(pathIsWalkable)
+    // println(sectionsMap)
+    // Answer.answer(contiguousSections)
+    Unit
+  }
+
+  def pathIsWalkable(vec: Vector[Vector[Int]]): Boolean = {
+    // val (startPos, rest) = vec.splitAt(1)
+    // println(startPos)
+    // println(rest)
+    // println(vec)
+    // val vectorMap = getSectionMap(rest)
+    findCircuit(vec)
+    false
+  }
+
+  def findCircuit(vec: Vector[Vector[Int]]): Unit = {
+
+    def isConnected(p1: Vector[Int], p2: Vector[Int]): Boolean = {
+      p1(1) == p2(0)
+    }
+
+    def findClosedCircuit(
+      origin: Vector[Int],
+      currentPoint: Vector[Int],
+      currentPath: Vector[Vector[Int]],
+      remainingPaths: Vector[Vector[Int]]): Unit = {
+        val linkToOrigin = paths.find(x => isConnected(x, origin))
+        if (linkToOrigin.isEmpty) {
+          // val linkToNext = remainingPaths.find(x => isConnected(x, currentPath))
+        } else {
+          // currentPath ++ link
+        }
+    }
+
+    val closedCircuit = mapPaths.find(x => x._1 == startPos)
+    println("closed, circuit", closedCircuit)
+    // Vector(Vector(1))
+
   }
 
   def parseStdin(): Vector[Vector[Vector[Int]]] = {
@@ -48,29 +89,11 @@ object EntryPoint {
     wordsToVector
   }
 
-  def isContiguous(vec: Vector[Vector[Int]]): Boolean = {
+  def getSectionMap(vec: Vector[Vector[Int]]): Map[Vector[Int], Int] = {
 
-    val maximumAbsoluteDifference = 2
+    val sectionMap = vec.groupBy(identity).mapValues(_.size)
 
-    val emptyMap = ('a'.toInt to 'z'.toInt).map(x => (x, 0)).toMap
-    val countStart = (
-      emptyMap ++ vec.map( x => x(0)).groupBy(identity).mapValues(_.size))
-    val countEnd = (
-      emptyMap ++ vec.map( x => x(1)).groupBy(identity).mapValues(_.size))
-    // println(vec)
-    // println(countStart)
-    // println(countEnd)
-
-    val diffMap = countEnd.map{case (x, y) => (x,  y - countStart(x))}
-    // println(diff)
-
-    val diffNonZero = diffMap.filter(x => x._2 != 0)
-    // println(diffNonZero)
-
-    val flatDifference = diffNonZero.values.map(scala.math.abs).sum
-    // println(flatDifference)
-
-    flatDifference <= maximumAbsoluteDifference
+    sectionMap
 
   }
-}
+} // --- }}}
