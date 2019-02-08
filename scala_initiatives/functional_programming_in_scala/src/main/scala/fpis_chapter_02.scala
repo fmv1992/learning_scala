fba4ae24876759413552090234ebc6d515c515b1
Felipe M. Vieira <fmv1992@gmail.com>
2019-02-08 11:13:50 -0200

    finished exercise 2.1; set template to all others

--- scala_initiatives/functional_programming_in_scala/src/main/scala/fpis_chapter_02.scala
+++ scala_initiatives/functional_programming_in_scala/src/main/scala/fpis_chapter_02.scala
@@ -4,6 +4,37 @@ import scalainitiatives.common.ScalaInitiativesExercise
 
 object FPISExerciseChapter02 extends ScalaInitiativesExercise {
 
-  println("hello day")
+  def Exercise2Dot1Fibonacci(n: Int) : Int = {
+
+    // @annotation.tailrec
+    def fib(n:Int, acc:Int): Int = {
+      if (n == 1) 1 else {
+        if (n == 0) 0 else {
+          fib(n-1, 0) + fib(n-2, 0)
+        }
+      }
+    }
+    fib(n, 0)
+  }
+
+  def Exercise2Dot2isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+
+    def go(leftPosition: Int): Boolean = {
+      // println(leftPosition, as, as(0))
+      if (as.length == 1 || as.length == 0) true else {
+        if (leftPosition == as.length) true else {
+          if (ordered(as(leftPosition), as(leftPosition + 1))) go(leftPosition + 1)
+          else false
+        }
+      }
+    }
+
+    go(0)
+  }
+  def Exercise2Dot3() {}
+  def Exercise2Dot4() {}
+  def Exercise2Dot5() {}
 
 }
+
+// vim: set filetype=scala fileformat=unix wrap tabstop=2 softtabstop=2:
--- scala_initiatives/functional_programming_in_scala/src/test/scala/fpis_chapter_test_02.scala
+++ scala_initiatives/functional_programming_in_scala/src/test/scala/fpis_chapter_test_02.scala
@@ -5,6 +5,36 @@ import scalainitiatives.common.ScalaInitiativesTest
 // ???
 import org.scalatest._
 
-class FPISTestChapter02 extends FlatSpec with Matchers with ScalaInitiativesTest {
-  FPISExerciseChapter02
+class FPISTestChapter02 extends FunSuite with Matchers with ScalaInitiativesTest {
+
+  // Here we have F(0) == 0.
+  test ("2.1: Fibonacci") {
+    val x = List(0, 1, 2, 7, 10, 30)
+    val truth = List(0, 1, 1, 13, 55, 832040)
+    val computed = x.map(FPISExerciseChapter02.Exercise2Dot1Fibonacci)
+    println(truth)
+    println(computed)
+    assert(computed == truth)
+  }
+
+  // test ("2.2: is Sorted") {
+  //   val pairs01 = List(
+  //     (Array(0), true),
+  //     (Array(100), true),
+  //     (Array(1, 2), true),
+  //     (Array(2, 2), true),
+  //     (Array(3, 2), false),
+  //     (Array(1, 2, 3, 2), false),
+  //     (Array(1, 1, 1, -1), false),
+  //     (Array(1000, 9, 1, -1), false),
+  //     )
+  //   val lists = pairs01.map(x => x._1)
+  //   val bools = pairs01.map(x => x._2)
+  //   // FPISExerciseChapter02.Exercise2Dot2isSorted
+  //   val sortFunction = (a: Int, b: Int) => a < b
+  //   val computed = lists.map(x => FPISExerciseChapter02.Exercise2Dot2isSorted(
+  //     x,
+  //     sortFunction))
+  // }
+
 }
