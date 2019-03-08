fc2910a4cf02ea93faa2c2d55078bc2d0e50bffa
Felipe M. Vieira <fmv1992@gmail.com>
2019-03-08 14:43:22 -0300

    setup complete for chapter 05 :)

---
+++ scala_initiatives/functional_programming_in_scala/src/main/scala/fpis_chapter_05.scala
@@ -0,0 +1,35 @@
+package scalainitiatives.functional_programming_in_scala
+
+import scala.{Stream => _, _}
+
+import scalainitiatives.common.ScalaInitiativesExercise
+
+object FPISExerciseChapter05 extends ScalaInitiativesExercise {
+
+  // From fpinscala <https://github.com/fpinscala/fpinscala>. --------------|
+  // Changed those to fpinscala to proceed with certainty of correctness ---|
+  // (despite using a lot of tests). ---------------------------------------| {
+  sealed trait Stream[+A] {
+    val isCustomStream = true
+  }
+
+  case object Empty extends Stream[Nothing]
+  case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
+
+  object Stream {
+    def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
+      lazy val head = hd
+      lazy val tail = tl
+      Cons(() => head, () => tail)
+    }
+    def empty[A]: Stream[A] = Empty
+    def apply[A](as: A*): Stream[A] =
+      if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
+  }
+  // From fpinscala <https://github.com/fpinscala/fpinscala>. --------------|
+  // Changed those to fpinscala to proceed with certainty of correctness ---|
+  // (despite using a lot of tests). ---------------------------------------| }
+
+}
+
+// vim: set filetype=scala fileformat=unix foldmarker={,} nowrap tabstop=2 softtabstop=2:
---
+++ scala_initiatives/functional_programming_in_scala/src/test/scala/fpis_chapter_test_05.scala
@@ -0,0 +1,67 @@
+package scalainitiatives.functional_programming_in_scala
+
+import scala.{Stream => _, _}
+
+import FPISExerciseChapter05.Stream
+import FPISExerciseChapter05.Cons
+import FPISExerciseChapter05.Empty
+
+// import org.scalatest.Matchers
+
+import scalainitiatives.common.ScalaInitiativesTest
+
+// Se matchers here:
+// http://www.scalatest.org/user_guide/using_matchers#checkingObjectIdentity
+class FPISTestChapter05 extends ScalaInitiativesTest {
+
+  // Declare constants.
+  val s1: Stream[Int] = Stream(1)
+
+  test ("5.0: Basic tests.") {
+    assert(s1.isCustomStream)
+  }
+
+  test("5.1: ???.") {
+
+  }
+
+  test("5.2: ???.") {
+
+  }
+
+  test("5.3: ???.") {
+
+  }
+
+  test("5.4: ???.") {
+
+  }
+
+  test("5.5: ???.") {
+
+  }
+
+  test("5.6: ???.") {
+
+  }
+
+  test("5.7: ???.") {
+
+  }
+
+  test("5.8: ???.") {
+
+  }
+
+  test("5.9: ???.") {
+
+  }
+
+
+}
+
+//  Run this in vim:
+//
+// vim source: iabbrev aa assert("5.x: ???.")
+//
+// vim: set filetype=scala fileformat=unix foldmarker={,} nowrap tabstop=2 softtabstop=2:
