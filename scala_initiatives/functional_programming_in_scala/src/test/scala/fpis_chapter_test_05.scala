db2d52b15aaf382c21e9c64d423872741f6d6d7a
Felipe M. Vieira <fmv1992@gmail.com>
2019-03-08 16:09:18 -0300

    added discussion to stream ++ method

--- scala_initiatives/functional_programming_in_scala/src/test/scala/fpis_chapter_test_05.scala
+++ scala_initiatives/functional_programming_in_scala/src/test/scala/fpis_chapter_test_05.scala
@@ -46,9 +46,45 @@ class FPISTestChapter05 extends ScalaInitiativesTest {
   }
 
   namedTest("5.a: Test ++.") {
-    // println(s2)
-    // assert((s2 ++ s2).toList == Stream(1, 2, 3, 1, 2, 3).toList)
-    s2 ++ s2 ++ s2
+    assert((s2 ++ s2).toList == Stream(1, 2, 3, 1, 2, 3).toList)
+    // Considering the commit '2ac328b', the command `make clean && make` gives
+    // us the following relevant portion of the code:
+    //
+    // [info] Done compiling.
+    // evalhead
+    // evaltail
+    // evalhead
+    // evaltail
+    // evalhead
+    // evaltail
+    // evalhead
+    // evaltail
+    // evalhead
+    // evaltail
+    // evalhead
+    // evaltail
+    // Starting: 5.a: Test ++. 25732014735933
+    // Ended:    5.a: Test ++. 25732015535532
+    // [info] FPISTestChapter05:
+    // [info] - 5.0.0: Basic tests.
+    // [info] - 5.0.1: Test memoization.
+    // [info] - 5.1: toList.
+    // [info] - 5.a: Test ++.
+    // [info] - 5.2: Implementation of take and drop.
+    // [info] - 5.3: Implementation of takeWhile.
+    // [info] - 5.4: ???.
+    // [info] - 5.5: ???.
+    // [info] - 5.6: ???.
+    // [info] - 5.7: ???.
+    // [info] - 5.8: ???.
+    // [info] - 5.9: ???.
+    //
+    // This shows us that the operator `++` keeps the lazyness of our Stream
+    // object. It thus helps us tremendously to define a Stream such as this:
+    //
+    // Stream(v1, v2, ..., {throw new Exception(); v3})
+    //
+    // This Stream will certainly help us in testing the lazy evaluation of our object at all times.
   }
 
   test("5.2: Implementation of take and drop.") {
