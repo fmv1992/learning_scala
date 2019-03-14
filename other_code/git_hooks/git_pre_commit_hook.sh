fa517bdf23fed98af750f3f4a6f28fc054239b3a
Felipe M. Vieira <fmv1992@gmail.com>
2019-03-14 16:34:40 -0300

    improved git pre commit hook to format commited/modified code

--- other_code/git_hooks/git_pre_commit_hook.sh
+++ other_code/git_hooks/git_pre_commit_hook.sh
@@ -17,6 +17,14 @@ else
     make clean_fpis_chapter
 fi
 
+git diff --name-only --cached \
+    | grep '\.scala' \
+    | parallel \
+        -I % \
+        --verbose \
+        --jobs $((2*$(nproc))) \
+        "vim -i NONE -c 'VimScalafmt' -c 'noautocmd x!' %"
+
 # https://stackoverflow.com/questions/3466166/how-to-check-if-running-in-cygwin-mac-or-linux
 cd ./scala_initiatives/
 if [[ $(uname -s) == CYGWIN* ]];then
@@ -27,5 +35,84 @@ else
     sbt testQuick
 fi
 
-# echo "Ret code $?"
-# exit 1
+Where the relevant vim code is:
+
+# Commit: 'c61d068' from vim_scala_plugin.
+#
+# augroup vim_scala_plugin
+#     autocmd!
+#     autocmd FileType scala command! -nargs=? VimScalafmt call s:VimScalafmt(<f-args>)
+# augroup END
+#
+# " ???: let g:vim_scala_plugin_scalafmt_file = ???
+#
+# function! s:VimScalafmt(...)
+#
+#     function! LocalSubstitute(str, pat, subs)
+#         return substitute(a:str, a:pat, a:subs, 'g')
+#     endfunction
+#
+#     " Set optional arguments.
+#     "
+#     " First argument is the path to the scalafmt config file.
+#     if a:0 == 0
+#         if exists("g:vim_scala_plugin_scalafmt_file")
+#             let l:conffile = g:vim_scala_plugin_scalafmt_file
+#         else
+#             let l:conffile = ''
+#         endif
+#     else
+#         let l:conffile = a:1
+#     endif
+#
+#     if l:conffile == ''
+#         let l:confcommand = ''
+#     else
+#         let l:confcommand = '--config ' . l:conffile . ' '
+#     endif
+#
+#     let l:scalafmt_command = "scalafmt "
+#         \ . l:confcommand
+#         \ . "--quiet "
+#         \ . "--non-interactive "
+#         \ . "--stdin "
+#         \ . "--stdout"
+#
+#     " let l:post_execution = "| "
+#     " let l:post_execution = ""
+#     let l:post_execution = " 2>/dev/null"
+#
+#     let l:execute_string = l:scalafmt_command . l:post_execution
+#     " let l:execute_string = shellescape(l:scalafmt_command . l:post_execution)
+#
+#     " echom l:post_execution_sed
+#     " echom l:execute_string
+#
+#     " ???: This is a workaround for:
+#     " https://github.com/scalameta/scalafmt/issues/1382
+#     " {
+#     let l:buffer_content = join(getline(1,'$'), "\n")
+#     let l:stdout = systemlist(l:execute_string, l:buffer_content)
+#
+#     if l:stdout == []
+#         echoerr "There were errors when running VimScalafmt."
+#         return -1
+#     endif
+#
+#     let l:joined_out = join(l:stdout, "\n")
+#
+#     " [[:space:]]
+#     let l:nobreak_after_arrows = substitute(
+#         \ l:joined_out,
+#         \ '⇒\n\s*',
+#         \ '⇒ ',
+#         \ 'g')
+#
+#     let l:nobreak_as_list = split(l:nobreak_after_arrows, "\n")
+#     " }
+#
+#     call setline(1, l:nobreak_as_list)
+#
+# endfunction
+
+# vim: set filetype=sh fileformat=unix wrap:
