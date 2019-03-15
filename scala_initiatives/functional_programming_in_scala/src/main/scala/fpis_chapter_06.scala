672b3e0a888ce4717d29ebdc1cbed1d090957670
Felipe M. Vieira <fmv1992@gmail.com>
2019-03-14 23:22:54 -0300

    setup for chapter 06

---
+++ scala_initiatives/functional_programming_in_scala/src/main/scala/fpis_chapter_06.scala
@@ -0,0 +1,30 @@
+package scalainitiatives.functional_programming_in_scala
+
+// import scala.{Stream ⇒ _, _}
+
+import scalainitiatives.common.ScalaInitiativesExercise
+
+// In order to use this interactively do:
+// |    $ sbt
+// |    sbt:LearningScala> project fpis
+// |
+// |    import scala.{Stream ⇒ _, _}
+// |    import scalainitiatives.functional_programming_in_scala.FPISExerciseChapter06._
+
+object FPISExerciseChapter06 extends ScalaInitiativesExercise {
+
+  // From fpinscala <https://github.com/fpinscala/fpinscala>. --------------|
+  // Changed those to fpinscala to proceed with certainty of correctness ---|
+  // (despite using a lot of tests). ---------------------------------------| {
+
+  // From fpinscala <https://github.com/fpinscala/fpinscala>. --------------|
+  // Changed those to fpinscala to proceed with certainty of correctness ---|
+  // (despite using a lot of tests). ---------------------------------------| }
+
+}
+
+//  Run this in vim:
+//
+// vim source: "
+//
+// vim: set filetype=scala fileformat=unix foldmarker={,} nowrap tabstop=2 softtabstop=2:
---
+++ scala_initiatives/functional_programming_in_scala/src/test/scala/fpis_chapter_test_06.scala
@@ -0,0 +1,68 @@
+package scalainitiatives.functional_programming_in_scala
+
+// import scala.{Stream ⇒ _, _}
+
+
+// import FPISExerciseChapter06.Stream
+
+import scalainitiatives.common.ScalaInitiativesTest
+
+// Se matchers here:
+// http://www.scalatest.org/user_guide/using_matchers#checkingObjectIdentity
+class FPISTestChapter06 extends ScalaInitiativesTest {
+
+  // Declare constants.
+  // ???.
+
+  test("6.1: ???.") {
+
+  }
+
+  test("6.2: ???.") {
+
+  }
+
+  test("6.3: ???.") {
+
+  }
+
+  test("6.4: ???.") {
+
+  }
+
+  test("6.5: ???.") {
+
+  }
+
+  test("6.6: ???.") {
+
+  }
+
+  test("6.7: ???.") {
+
+  }
+
+  test("6.8: ???.") {
+
+  }
+
+  test("6.9: ???.") {
+
+  }
+
+  test("6.10: ???.") {
+
+  }
+
+  test("6.11: ???.") {
+
+  }
+
+
+
+}
+
+
+//  Run this in vim:
+//
+// vim: set filetype=scala fileformat=unix foldmarker={,} nowrap tabstop=2 softtabstop=2:
