4c3f0ee7f366cca5d85ddd9d3a0ba7e04c9f033e
Felipe M. Vieira <fmv1992@gmail.com>
2019-03-15 17:11:08 -0300

    solved fpis until exercise 6.1

--- scala_initiatives/functional_programming_in_scala/src/main/scala/fpis_chapter_06.scala
+++ scala_initiatives/functional_programming_in_scala/src/main/scala/fpis_chapter_06.scala
@@ -24,21 +24,48 @@ object FPISExerciseChapter06 extends ScalaInitiativesExercise {
   case class SimpleRNG(seed: Long) extends RNG {
 
     def nextInt: (Int, RNG) = {
-      val newSeed = (seed * 0X5DEECE66DL + 0XBL) & 0XFFFFFFFFFFFFL
+      // https://en.wikipedia.org/wiki/Linear_congruential_generator
+      // Ints are from  range:
+      val modulus = 0XFFFFFFFFFFFFL
+      val multiplier = 0X5DEECE66DL
+      val increment = 0XBL
+      val newSeed = (seed * multiplier + increment) & modulus
+      //  ↑↑↑↑↑↑↑ → Max value of: 0XFFFFFFFFFFFFL - 1L = 281474976710654L
+      // scala> Int.MaxValue.toLong * 2 + 1L == 0XFFFFFFFFFFFFL >>> 16
+      // res35: Boolean = true
+
       val nextRNG = SimpleRNG(newSeed)
+
+      // ">>> is right binary shift with zero fill."
+      //
+      // scala> (0XFFFFFFFFFFFFL >>> 16) / Int.MaxValue.toDouble
+      // res29: Double = 2.0000000004656613
       val n = (newSeed >>> 16).toInt
       (n, nextRNG)
     }
+
+    // From fpinscala <https://github.com/fpinscala/fpinscala>. ------------|
+    // Changed those to fpinscala to proceed with certainty of correctness -|
+    // (despite using a lot of tests). -------------------------------------| }
+
   }
 
-  // From fpinscala <https://github.com/fpinscala/fpinscala>. --------------|
-  // Changed those to fpinscala to proceed with certainty of correctness ---|
-  // (despite using a lot of tests). ---------------------------------------| }
+  object SimpleRNG {
+
+    // NOTE: see (note6.1).
+    def nonNegativeInt(rng: RNG): (Int, RNG) = {
+      val (n, ns) = rng.nextInt
+      val nni = math.abs(n)
+      if (nni < 0) nonNegativeInt(ns) else (nni, ns)
+    }
+
+  }
 
 }
 
 //  Run this in vim:
 //
-// vim source: "
+// ???: Why this is not automatic? It should be.
+// vim source: iabbrev t the
 //
 // vim: set filetype=scala fileformat=unix foldmarker={,} nowrap tabstop=2 softtabstop=2:
--- scala_initiatives/functional_programming_in_scala/src/test/scala/fpis_chapter_test_06.scala
+++ scala_initiatives/functional_programming_in_scala/src/test/scala/fpis_chapter_test_06.scala
@@ -2,8 +2,7 @@ package scalainitiatives.functional_programming_in_scala
 
 // import scala.{Stream ⇒ _, _}
 
-
-// import FPISExerciseChapter06.Stream
+import FPISExerciseChapter06.SimpleRNG
 
 import scalainitiatives.common.ScalaInitiativesTest
 
@@ -14,55 +13,40 @@ class FPISTestChapter06 extends ScalaInitiativesTest {
   // Declare constants.
   // ???.
 
-  test("6.1: ???.") {
-
+  test("6.1: Implementation of nonNegativeInt.") {
+    // NOTE: (note6.1) There is no information regarding the range of return of
+    // nextInt. Thus it is not clear how we should proceed in mapping the new
+    // value.
+    assert(
+      Stream
+        .from(0)
+        .take(100)
+        .map(SimpleRNG(_))
+        .forall(x ⇒ SimpleRNG.nonNegativeInt(x)._1 >= 0)
+    )
   }
 
-  test("6.2: ???.") {
+  test("6.2: ???.") {}
 
-  }
+  test("6.3: ???.") {}
 
-  test("6.3: ???.") {
+  test("6.4: ???.") {}
 
-  }
+  test("6.5: ???.") {}
 
-  test("6.4: ???.") {
+  test("6.6: ???.") {}
 
-  }
+  test("6.7: ???.") {}
 
-  test("6.5: ???.") {
+  test("6.8: ???.") {}
 
-  }
+  test("6.9: ???.") {}
 
-  test("6.6: ???.") {
+  test("6.10: ???.") {}
 
-  }
-
-  test("6.7: ???.") {
-
-  }
-
-  test("6.8: ???.") {
-
-  }
-
-  test("6.9: ???.") {
-
-  }
-
-  test("6.10: ???.") {
-
-  }
-
-  test("6.11: ???.") {
-
-  }
-
-
+  test("6.11: ???.") {}
 
 }
-
-
 //  Run this in vim:
 //
 // vim: set filetype=scala fileformat=unix foldmarker={,} nowrap tabstop=2 softtabstop=2:
