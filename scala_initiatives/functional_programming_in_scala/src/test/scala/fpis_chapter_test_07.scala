1c6959a904490e63c0019d8406b77e3dd640e8d4
Felipe M. Vieira <fmv1992@gmail.com>
2019-04-08 16:00:07 -0300

    added template for chapter 07

---
+++ scala_initiatives/functional_programming_in_scala/src/test/scala/fpis_chapter_test_07.scala
@@ -0,0 +1,39 @@
+package scalainitiatives.functional_programming_in_scala
+
+import FPISExerciseChapter07._
+
+import scalainitiatives.common.ScalaInitiativesTest
+
+// import org.scalatest.Matchers
+
+// Se matchers here:
+// http://www.scalatest.org/user_guide/using_matchers#checkingObjectIdentity
+class FPISTestChapter07 extends ScalaInitiativesTest with Matchers {
+
+  // Declare constants.
+
+  test("7.1: ???.") {}
+  test("7.2: ???.") {}
+  test("7.3: ???.") {}
+  test("7.4: ???.") {}
+  test("7.5: ???.") {}
+  test("7.6: ???.") {}
+  test("7.7: ???.") {}
+  test("7.8: ???.") {}
+  test("7.9: ???.") {}
+  test("7.10: ???.") {}
+  test("7.11: ???.") {}
+  test("7.12: ???.") {}
+  test("7.13: ???.") {}
+  test("7.14: ???.") {}
+
+}
+
+//  Run this in vim:
+//
+// vim source: iabbrev R RNG
+// vim source: iabbrev Si SimpleRNG
+// vim source: 1,-10s/⇒/⇒/ge
+// vim source: NeoCompleteEnable
+//
+// vim: set filetype=scala fileformat=unix foldmarker={,} nowrap tabstop=2 softtabstop=2:
