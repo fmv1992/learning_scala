842d871bdcc07785e358b0fb780dddb1b82a15cd
Felipe M. Vieira <fmv1992@gmail.com>
2019-04-09 11:07:33 -0300

    reproduced Listing 7.5 Basic implementation for Par

--- scala_initiatives/functional_programming_in_scala/src/main/scala/fpis_chapter_07.scala
+++ scala_initiatives/functional_programming_in_scala/src/main/scala/fpis_chapter_07.scala
@@ -31,18 +31,36 @@ object FPISExerciseChapter07 extends ScalaInitiativesExercise {
 
   object Par {
 
-    def unit[A](a: => A): Par[A] = {
-      ???
-    }
-
     def run[A](a: Par[A]): A = {
       ???
     }
 
-    def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) ⇒ C): Par[C] = {
-      ???
+    def unit[A](a: A): Par[A] = (es: ExecutorService) => UnitFuture(a)
+
+    private case class UnitFuture[A](get: A) extends Future[A] {
+      def isDone = true
+      def get(timeout: Long, units: TimeUnit) = get
+      def isCancelled = false
+      def cancel(evenIfRunning: Boolean): Boolean = false
     }
-    // type TimeUnit
+
+    def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] = {
+      (es: ExecutorService) =>
+        {
+          val af = a(es)
+          val bf = b(es)
+          UnitFuture(f(af.get, bf.get))
+        }
+    }
+
+    def fork[A](a: => Par[A]): Par[A] = {
+      es =>
+        es.submit(
+          new Callable[A] {
+            def call = a(es).get
+          }
+        )
+    }
 
   }
 
@@ -61,29 +79,19 @@ object FPISExerciseChapter07 extends ScalaInitiativesExercise {
 
   trait Future[A] {
 
-    def get: A = {
-      ???
-    }
+    def get: A
 
-    def get(timeout: Long, unit: TimeUnit): A = {
-      ???
-    }
+    def get(timeout: Long, unit: TimeUnit): A
 
-    def cancel(evenIfRunning: Boolean): Boolean = {
-      ???
-    }
+    def cancel(evenIfRunning: Boolean): Boolean
 
-    def isDone: Boolean = {
-      ???
-    }
+    def isDone: Boolean
 
-    def isCancelled: Boolean = {
-      ???
-    }
-
-    def run[A](s: ExecutorService)(a: Par[A]): Future[A] = a(s)
+    def isCancelled: Boolean
 
   }
+
+  def run[A](s: ExecutorService)(a: Par[A]): Future[A] = a(s)
 
   // Parallel computation. --- }
 }
