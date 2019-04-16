3396b86d3a74466e3e2e2a279650b30ed73bc265
Felipe M. Vieira <fmv1992@gmail.com>
2019-04-16 09:52:47 -0300

    [fpis]: corrected chapter 01 and 02

--- scala_initiatives/functional_programming_in_scala/src/main/scala/fpis_chapter_01.scala
+++ scala_initiatives/functional_programming_in_scala/src/main/scala/fpis_chapter_01.scala
@@ -2,7 +2,6 @@ package scalainitiatives.functional_programming_in_scala
 
 import scalainitiatives.common.ScalaInitiativesExercise
 
+// This file corrected against `fpinscala`.
 
-object FPISExerciseChapter01 extends ScalaInitiativesExercise {
-
-}
+object FPISExerciseChapter01 extends ScalaInitiativesExercise {}
--- scala_initiatives/functional_programming_in_scala/src/main/scala/fpis_chapter_02.scala
+++ scala_initiatives/functional_programming_in_scala/src/main/scala/fpis_chapter_02.scala
@@ -2,9 +2,11 @@ package scalainitiatives.functional_programming_in_scala
 
 import scalainitiatives.common.ScalaInitiativesExercise
 
+// This file corrected against `fpinscala`.
+
 object FPISExerciseChapter02 extends ScalaInitiativesExercise {
 
-  def Exercise2Dot1Fib(n: Int) : Int = {
+  def Exercise2Dot1Fib(n: Int): Int = {
 
     // F(n) = F(n-1) + F(n-2)
     // Example:
@@ -13,8 +15,7 @@ object FPISExerciseChapter02 extends ScalaInitiativesExercise {
     // F(4) = F(3) + F(2)
     def fib(n: Int): Int = {
       @annotation.tailrec
-      def go(fibprev1: Int, fibprev2: Int, position: Int): Int =
-      {
+      def go(fibprev1: Int, fibprev2: Int, position: Int): Int = {
         val newv = fibprev1 + fibprev2
         if (position == n) newv
         else {
@@ -28,12 +29,18 @@ object FPISExerciseChapter02 extends ScalaInitiativesExercise {
 
   }
 
-  def Exercise2Dot2isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+  def Exercise2Dot2isSorted[A](
+      as: Array[A],
+      ordered: (A, A) => Boolean
+  ): Boolean = {
 
     def go(leftPosition: Int): Boolean = {
-      if (as.length == 1 || as.length == 0) true else {
-        if (leftPosition == as.length - 1) true else {
-          if (ordered(as(leftPosition), as(leftPosition + 1))) go(leftPosition + 1)
+      if (as.length == 1 || as.length == 0) true
+      else {
+        if (leftPosition == as.length - 1) true
+        else {
+          if (ordered(as(leftPosition), as(leftPosition + 1)))
+            go(leftPosition + 1)
           else false
         }
       }
@@ -44,16 +51,19 @@ object FPISExerciseChapter02 extends ScalaInitiativesExercise {
 
   def Exercise2Dot3Currying[A, B, C](f: (A, B) => C): A => (B => C) = {
     (a: A) => (
-      (b: B) => f(a, b)
+        (b: B) =>
+          f(a, b)
     )
   }
 
   def Exercise2Dot4Uncurrying[A, B, C](f: A => B => C): (A, B) => C = {
-    (a: A, b: B) => f(a)(b)
+    (a: A, b: B) =>
+      f(a)(b)
   }
 
   def Exercise2Dot5Compose[A, B, C](f: B => C, g: A => B): A => C = {
-    x => f(g(x))
+    x =>
+      f(g(x))
   }
 
 }
