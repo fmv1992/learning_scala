05a928b166e53abf211ac4fdbcd6df927193f89b
Felipe M. Vieira <fmv1992@gmail.com>
2019-04-16 10:06:45 -0300

    [fpis]: corrected chapter 04

--- scala_initiatives/functional_programming_in_scala/src/main/scala/fpis_chapter_04.scala
+++ scala_initiatives/functional_programming_in_scala/src/main/scala/fpis_chapter_04.scala
@@ -7,7 +7,6 @@ import scala.{Option => _, _}
 import scala.{Right => _, _}
 import scala.{Some => _, _}
 
-
 import scalainitiatives.common.ScalaInitiativesExercise
 
 // In order to use this interactively do:
@@ -40,24 +39,24 @@ object FPISExerciseChapter04 extends ScalaInitiativesExercise {
     //    5. `filter`.
 
     def map[B](f: A => B): Option[B] = this match {
-      case None => None
+      case None    => None
       case Some(a) => Some(f(a))
     }
 
     // ???: Supertype.
-    def getOrElse[B>:A](default: => B): B = this match {
-      case None => default
+    def getOrElse[B >: A](default: => B): B = this match {
+      case None    => default
       case Some(a) => a
     }
 
     def flatMap[B](f: A => Option[B]): Option[B] = map(f) getOrElse None
 
-    def orElse[B>:A](ob: => Option[B]): Option[B] =
+    def orElse[B >: A](ob: => Option[B]): Option[B] =
       this map (Some(_)) getOrElse ob
 
     def filter(f: A => Boolean): Option[A] = this match {
       case Some(a) if f(a) => this
-      case _ => None
+      case _               => None
     }
 
     def lift[A, B](f: A => B): Option[A] => Option[B] = _ map f
@@ -74,24 +73,26 @@ object FPISExerciseChapter04 extends ScalaInitiativesExercise {
     // option which is then flatmapped:
     // mean(xs) flatMap (m => mean(xs.map(x => math.pow(x - m, 2))))
     def mean(x: Seq[Double]): Double = x.sum / x.length
+
     def vari(x: Seq[Double]): Double = {
       val xsMean = mean(x)
       mean(x.map(z => math.pow(z - xsMean, 2)))
     }
+
     def variance(xs: Seq[Double]): Option[Double] = {
-      Some(xs).flatMap(
-        z => if (xs.length == 0) None else Some(vari(z)))
+      Some(xs).flatMap(z => if (xs.length == 0) None else Some(vari(z)))
     }
 
     // IMPROVEMENT: can be done in terms of flatmap. Notice that these pattern
     // matches are part of map (which is part of flatmap).
-    def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = {
+    def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = {
       a match {
         case None => None
-        case Some(x) => b match {
-          case None => None
-          case Some(y) => Some(f(x, y))
-        }
+        case Some(x) =>
+          b match {
+            case None    => None
+            case Some(y) => Some(f(x, y))
+          }
       }
     }
 
@@ -103,37 +104,41 @@ object FPISExerciseChapter04 extends ScalaInitiativesExercise {
     //
     // The problem here is that the list has to transverse it entirely even if
     // the first value is None.
+    // ??!: Correction: Use flatmap with sequence (recursive).
     def sequence[A](a: List[Option[A]]): Option[List[A]] = {
       def go(x: List[Option[A]], acc: Option[List[A]]): Option[List[A]] = {
         acc match {
-          case None => None                // Option[List[A]]: Stop recursion.
-          case Some(lacc) => x match {
-            case Nil => acc                  // Option[List[A]]
-            case h :: t => h match {
-              case None => None                // Option[List[A]]
-              case Some(y) => go(t, Some(y :: lacc))
+          case None => None // Option[List[A]]: Stop recursion.
+          case Some(lacc) =>
+            x match {
+              case Nil => acc // Option[List[A]]
+              case h :: t =>
+                h match {
+                  case None    => None // Option[List[A]]
+                  case Some(y) => go(t, Some(y :: lacc))
+                }
             }
-          }
         }
       }
       go(a, Some(Nil))
     }
 
-
+    // ??!: Correction: Recursive solution is much better.
     def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = {
       def go(x: List[A], acc: Option[List[B]]): Option[List[B]] = {
         acc match {
           case None => None
-          case Some(lacc) => x match {
-            case Nil => acc
-            case h :: t => {
-              val funcH = f(h)
-              funcH match {
-                case None => None
-                case Some(y) => go(t, Some(y :: lacc))
+          case Some(lacc) =>
+            x match {
+              case Nil => acc
+              case h :: t => {
+                val funcH = f(h)
+                funcH match {
+                  case None    => None
+                  case Some(y) => go(t, Some(y :: lacc))
+                }
               }
             }
-          }
         }
       }
       go(a, Some(Nil))
@@ -145,8 +150,6 @@ object FPISExerciseChapter04 extends ScalaInitiativesExercise {
     }
 
   }
-
-
 
   // def Try[A](a: => A): Either[Exception, A] = {
   // try Right(a)
@@ -161,7 +164,6 @@ object FPISExerciseChapter04 extends ScalaInitiativesExercise {
   // }
   // }
 
-
   trait Either[+E, +A] {
 
     val isCustomEither = true
@@ -175,28 +177,27 @@ object FPISExerciseChapter04 extends ScalaInitiativesExercise {
 
     def map[B](f: A => B): Either[E, B] = {
       this match {
-        case Left(x) => Left(x)
+        case Left(x)  => Left(x)
         case Right(x) => Right(f(x))
       }
     }
 
     def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = {
       this match {
-        case Left(x) => Left(x)
+        case Left(x)  => Left(x)
         case Right(x) => f(x)
       }
     }
 
-    def orElse[EE >: E,B >: A](b: => Either[EE, B]): Either[EE, B] = {
+    def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = {
       this match {
-        case Left(x) => b
+        case Left(x)  => b
         case Right(x) => Right(x)
       }
     }
 
     def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = {
-      this.flatMap(aa =>
-          b.map(bb => f(aa, bb)))
+      this.flatMap(aa => b.map(bb => f(aa, bb)))
     }
 
   }
@@ -204,20 +205,27 @@ object FPISExerciseChapter04 extends ScalaInitiativesExercise {
   object Either {
 
     def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]] = {
-      def go(l: List[Either[E, A]], acc: Either[E, List[A]]): Either[E, List[A]] = {
+      def go(
+          l: List[Either[E, A]],
+          acc: Either[E, List[A]]
+      ): Either[E, List[A]] = {
         l match {
           case Nil ⇒ acc
           case h :: t ⇒ h match {
-            case Left(x) ⇒ Left(x)
-            case Right(x) ⇒ go(t, acc.map(z => List(x) ++ z))
-          }
+              case Left(x) ⇒ Left(x)
+              case Right(x) ⇒ go(t, acc.map(z => List(x) ++ z))
+            }
         }
       }
       // go(es.reverse, Right(Nil)).flatMap(x => Right(x.reverse))
       go(es.reverse, Right(Nil))
     }
 
-    def traverse[E, A, B](as: List[A])(f: A => Either[E, B]): Either[E, List[B]] = {
+    // ???
+    // ??!: Correction: Use map2 and traverse.
+    def traverse[E, A, B](
+        as: List[A]
+    )(f: A => Either[E, B]): Either[E, List[B]] = {
       def go(l: List[A], acc: Either[E, List[B]]): Either[E, List[B]] = {
         l match {
           case Nil ⇒ acc
@@ -235,16 +243,15 @@ object FPISExerciseChapter04 extends ScalaInitiativesExercise {
 
   }
 
-              case class Left[+E](value: E) extends Either[E, Nothing]
-              case class Right[+A](value: A) extends Either[Nothing, A]
+  case class Left[+E](value: E) extends Either[E, Nothing]
+  case class Right[+A](value: A) extends Either[Nothing, A]
 
-              // object Either {
-              // def Try[A](a: => A): Either[Exception, A] =
-              // try Right(a)
-              // catch { case e: Exception => Left(e) }
-              // }
-
+  // object Either {
+  // def Try[A](a: => A): Either[Exception, A] =
+  // try Right(a)
+  // catch { case e: Exception => Left(e) }
+  // }
 
 }
 
-// vim: set filetype=scala fileformat=unix foldmarker={,} nowrap tabstop=2 softtabstop=2:
+// vim: set filetype=scala fileformat=unix foldmarker={,} nowrap tabstop=2 softtabstop=2 spell:
