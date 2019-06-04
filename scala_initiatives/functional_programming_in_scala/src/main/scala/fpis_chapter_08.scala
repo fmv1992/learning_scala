872f348ef64d4fb5f5a96c4ba0892cdb4bc68eff
Felipe M. Vieira <fmv1992@gmail.com>
2019-06-04 11:09:35 -0300

    [fpis]: solved exercise 8.4

--- scala_initiatives/functional_programming_in_scala/src/main/scala/fpis_chapter_08.scala
+++ scala_initiatives/functional_programming_in_scala/src/main/scala/fpis_chapter_08.scala
@@ -20,6 +20,41 @@ object FPISExerciseChapter08 extends ScalaInitiativesExercise {
 
   }
 
+  type State[S, +A] = S ⇒ (S, A)
+
+  case class PRNG(seed: Long)
+
+  object PRNG {
+
+    def apply(a: Int): PRNG = PRNG(a.toLong)
+
+    def nextInt: State[PRNG, Int] = {
+      (prng: PRNG) ⇒ {
+          val newSeed = (prng.seed * 0X5DEECE66DL + 0XBL) & 0XFFFFFFFFFFFFL
+          val nextRNG = PRNG(newSeed)
+          val n = (newSeed >>> 16).toInt
+          (nextRNG, n)
+        }
+    }
+
+  }
+
+  case class Gen[B](sample: State[PRNG, B])
+
+  object Gen {
+
+    def choose(start: Int, stopExclusive: Int): Gen[Int] = {
+      Gen((x: PRNG) ⇒ {
+        // val randomSeed = System.nanoTime
+        val randomSeed = 0
+        val (p, ni) = PRNG.nextInt(PRNG(randomSeed))
+        val newInt = ni % (stopExclusive - start) + start
+        (p, newInt)
+      })
+    }
+
+  }
+
 }
 
 //  Run this in vim:
