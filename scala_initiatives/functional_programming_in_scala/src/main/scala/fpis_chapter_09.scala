854ea226126c1594e451eacef7dd9cd5f97be663
Felipe M. Vieira <fmv1992@gmail.com>
2019-06-06 20:29:07 -0300

    [fpis]: added skeleton to chapter 09

---
+++ scala_initiatives/functional_programming_in_scala/src/main/scala/fpis_chapter_09.scala
@@ -0,0 +1,11 @@
+package scalainitiatives.functional_programming_in_scala
+
+import scalainitiatives.common.ScalaInitiativesExercise
+
+object FPISExerciseChapter09 extends ScalaInitiativesExercise {}
+
+// Run this in vim:
+//
+// vim source: 1,$-10s/=>/⇒/ge
+//
+// vim: set filetype=scala fileformat=unix foldmarker={,} nowrap tabstop=2 softtabstop=2 spell spelllang=en:
---
+++ scala_initiatives/functional_programming_in_scala/src/test/scala/fpis_chapter_test_09.scala
@@ -0,0 +1,107 @@
+package scalainitiatives.functional_programming_in_scala
+
+import FPISExerciseChapter09._
+
+import scalainitiatives.common.ScalaInitiativesTest
+
+class FPISTestChapter09 extends ScalaInitiativesTest {
+
+  // Declare constants.
+
+  // Base64 hint for exercise 9.1:
+  // VHJ5IG1hcHBpbmcgb3ZlciB0aGUgcmVzdWx0IG9mIGBwcm9kdWN0YC4=
+  test("9.1: ???.") {}
+
+  // Base64 hint for exercise 9.2:
+  // TXVsdGlwbGljYXRpb24gb2YgbnVtYmVycyBpcyBhc3NvY2lhdGl2ZSwgYGEgKiAoYiAqIGMpID09
+  // IChhICogYikgKiBjYC4gSXMgdGhlcmUgYW4gYW5hbG9nb3VzIHByb3BlcnR5IGZvciBwYXJzZXJz
+  // PyBXaGF0IGNhbiB5b3Ugc2F5IGFib3V0IHRoZSByZWxhdGlvbnNoaXAgYmV0d2VlbiBgbWFwYCBh
+  // bmQgYHByb2R1Y3RgPwo=
+  test("9.2: ???.") {}
+
+  // Base64 hint for exercise 9.3:
+  // LQo=
+  test("9.3: ???.") {}
+
+  // Base64 hint for exercise 9.4:
+  // LQ==
+  test("9.4: ???.") {}
+
+  // Base64 hint for exercise 9.5:
+  // LQ==
+  test("9.5: ???.") {}
+
+  // Base64 hint for exercise 9.6:
+  // R2l2ZW4gYSBzdHJpbmcgb2YgZGlnaXRzLCBgc2AsIHlvdSBjYW4gdXNlIGBzLnRvSW50YCB0byBj
+  // b252ZXJ0IHRoYXQgdG8gYW4gYEludGAuCg==
+  test("9.6: ???.") {}
+
+  // Base64 hint for exercise 9.7:
+  // VXNlIGBmbGF0TWFwYCBhbmQgYHN1Y2NlZWRgLg==
+  test("9.7: ???.") {}
+
+  // Base64 hint for exercise 9.8:
+  // LQo=
+  test("9.8: ???.") {}
+
+  // Base64 hint for exercise 9.9:
+  // Rm9yIHRoZSB0b2tlbnMgb2YgeW91ciBncmFtbWFyLCBpdCdzIG9mdGVuIGEgZ29vZCBpZGVhIHRv
+  // IHNraXAgYW55IHRyYWlsaW5nIHdoaXRlc3BhY2UsIHRvIGF2b2lkIGhhdmluZyB0byBkZWFsIHdp
+  // dGggd2hpdGVzcGFjZSBldmVyeXdoZXJlIGluIHlvdXIgZ3JhbW1hci4gVHJ5IGludHJvZHVjaW5n
+  // IGEgY29tYmluYXRvciBmb3IgdGhpcy4gCgpXaGVuIHNlcXVlbmNpbmcgcGFyc2VycyB3aXRoIGAq
+  // KmAsIGl0J3MgY29tbW9uIHRvIHdhbnQgdG8gaWdub3JlIG9uZSBvZiB0aGUgcGFyc2VycyBpbiB0
+  // aGUgc2VxdWVuY2UsIGFuZCB5b3UnbGwgcHJvYmFibHkgd2FudCB0byBpbnRyb2R1Y2UgY29tYmlu
+  // YXRvcnMgZm9yIHRoaXMu
+  test("9.9: ???.") {}
+
+  // Base64 hint for exercise 9.10:
+  // LQ==
+  test("9.10: ???.") {}
+
+  // Base64 hint for exercise 9.11:
+  // SGVyZSBhcmUgdHdvIG9wdGlvbnM6IHdlIGNvdWxkIHJldHVybiB0aGUgbW9zdCByZWNlbnQgZXJy
+  // b3IgaW4gdGhlIGBvcmAgY2hhaW4sIG9yIHdlIGNvdWxkIHJldHVybiB3aGljaGV2ZXIgZXJyb3Ig
+  // b2NjdXJyZWQgYWZ0ZXIgZ2V0dGluZyBmdXJ0aGVzdCBpbnRvIHRoZSBpbnB1dCBzdHJpbmcu
+  test("9.11: ???.") {}
+
+  // Base64 hint for exercise 9.12:
+  // LQ==
+  test("9.12: ???.") {}
+
+  // Base64 hint for exercise 9.13:
+  // LQ==
+  test("9.13: ???.") {}
+
+  // Base64 hint for exercise 9.14:
+  // WW91IG1heSB3YW50IGBzdHJpbmdgIHRvIHJlcG9ydCB0aGUgaW1tZWRpYXRlIGNhdXNlIG9mIGZh
+  // aWx1cmUgKHdoaWNoZXZlciBjaGFyYWN0ZXIgZGlkbid0IG1hdGNoKSwgYXMgd2VsbCBhcyB0aGUg
+  // b3ZlcmFsbCBzdHJpbmcgYmVpbmcgcGFyc2VkLg==
+  test("9.14: ???.") {}
+
+  // Base64 hint for exercise 9.15:
+  // LQ==
+  test("9.15: ???.") {}
+
+  // Base64 hint for exercise 9.16:
+  // LQ==
+  test("9.16: ???.") {}
+
+  // Base64 hint for exercise 9.17:
+  // VHJ5IGFkZGluZyBhbm90aGVyIHBpZWNlIG9mIHN0YXRlIHRvIGBMb2NhdGlvbmAsIGBpc1NsaWNl
+  // ZGAuIFlvdSBtYXkgd2FudCB0byByZW5hbWUgYExvY2F0aW9uYCB0byBgUGFyc2VTdGF0ZWAsIGFz
+  // IGl0J3Mgbm8gbG9uZ2VyIGp1c3QgdGhlIGxvY2F0aW9uIQ==
+  test("9.17: ???.") {}
+
+  // Base64 hint for exercise 9.18:
+  // WW91IGNhbiBhZGQgYW4gYXR0cmlidXRlIGBvdGhlckZhaWx1cmVzOiBMaXN0W1BhcnNlRXJyb3Jd
+  // YCBvbiBgUGFyc2VFcnJvcmAgaXRzZWxmLiBUaGlzIHdpbGwgYmUgYSBsaXN0IG9mIHBhcnNlIGVy
+  // cm9ycyB0aGF0IG9jY3VycmVkIGluIG90aGVyIGJyYW5jaGVzIG9mIHRoZSBwYXJzZXIu
+  test("9.18: ???.") {}
+
+}
+
+// Run this in vim:
+//
+// vim source: 1,$-10s/=>/⇒/ge
+//
+// vim: set filetype=scala fileformat=unix foldmarker={,} nowrap tabstop=2 softtabstop=2 spell spelllang=en:
