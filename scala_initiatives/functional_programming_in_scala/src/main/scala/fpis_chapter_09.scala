a66cf44981f39a31e2bb597bf86363863b196487
Felipe M. Vieira <fmv1992@gmail.com>
2019-06-07 10:44:50 -0300

    [fpis]: read the book and added more code; could fix the map problem :) by defining a trait Parser

--- scala_initiatives/functional_programming_in_scala/src/main/scala/fpis_chapter_09.scala
+++ scala_initiatives/functional_programming_in_scala/src/main/scala/fpis_chapter_09.scala
@@ -9,16 +9,23 @@ import scalainitiatives.common.ScalaInitiativesExercise
 
 object FPISExerciseChapter09 extends ScalaInitiativesExercise {
 
-  trait Parsers[ParseError, Parser[+ _]] {
+  trait ParseError
+
+  trait Parser[+A] {
+    def map[B](f: A ⇒ B): Parser[B]
+  }
+
+  // trait Parsers[ParseError, Parser[+ _]] {
+  trait Parsers {
 
     // https://docs.scala-lang.org/tour/self-types.html
-    self ⇒ 
-    def run[A](p: Parser[A])(input: String): Either[ParseError, A]
-    def char(c: Char): Parser[Char]
+    self ⇒ // Dummy comment.
 
     def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]]
 
     def or[A](s1: Parser[A], s2: Parser[A]): Parser[A]
+
+    def run[A](p: Parser[A])(input: String): Either[ParseError, A]
 
     implicit def string(s: String): Parser[String]
     implicit def operators[A](p: Parser[A]) = ParserOps[A](p)
@@ -26,11 +33,37 @@ object FPISExerciseChapter09 extends ScalaInitiativesExercise {
         implicit f: A ⇒ Parser[String]
     ): ParserOps[String] = ParserOps(f(a))
 
+    // def map[A,B](a: Parser[A])(f: A ⇒ B): Parser[B]
+
     case class ParserOps[A](p: Parser[A]) {
       def |[B >: A](p2: Parser[B]): Parser[B] = self.or(p, p2)
       def or[B >: A](p2: ⇒ Parser[B]): Parser[B] = self.or(p, p2)
     }
+
+    def char(c: Char): Parser[Char] = string(c.toString) map (_.charAt(0))
+    def succeed[A](a: A): Parser[A] = string("") map (_ ⇒ a)
+
   }
+
+  // object Parsers {
+
+  //   // ???: These are actually defined in the trait. What's the meaning of that
+  //   // since they are used to instantiate a new parser?
+  //   //
+  //   // def char(c: Char): Parser[Char] = string(c.toString) map (_.charAt(0))
+  //   // def succeed[A](a: A): Parser[A] = string("") map (_ ⇒ a)
+
+  // }
+
+  // Currently it breaks code:
+  // object Laws {
+
+  //   def equal[A](p1: Parser[A], p2: Parser[A])(in: Gen[String]): Prop =
+  //     forAll(in)(s ⇒ run(p1)(s) == run(p2)(s))
+
+  //   def mapLaw[A](p: Parser[A])(in: Gen[String]): Prop =
+  //     equal(p, p.map(a => a))(in)
+  // }
 
 }
 
