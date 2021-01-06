81b37a5d170a8df810e4fdcaf9accd2908230c76
Felipe M. Vieira <fmv1992@gmail.com>
2021-01-06 17:47:40 -0300

    added failing test for dice game

--- scala_initiatives/project_euler/src/test/scala/Test_ID_0205_DiceGame.scala
+++ scala_initiatives/project_euler/src/test/scala/Test_ID_0205_DiceGame.scala
@@ -5,7 +5,7 @@ import org.scalatest.funsuite.AnyFunSuite
 class Test_ID_0205_DiceGame extends AnyFunSuite {
 
   test("Test solution.") {
-    assert(ID_0005_SmallestMultiple.core(1, 20) === 232792560)
+    assert(false)
   }
 
 }
